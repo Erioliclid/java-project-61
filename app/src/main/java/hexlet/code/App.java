c44b5485e8c707ba673dc63a0e5382b629322ff6
Erioliclid <erioliclid@gmail.com>
2022-08-30 21:41:55 +0300

    add second game

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,9 +3,26 @@
  */
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
-        Cli.greeting();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.print(
+                "1 - Greet\n" +
+                        "2 - Even\n" +
+                        "0 - Exit\n" +
+                        "Your choice: "
+        );
+        int answer = scanner.nextInt();
+        switch (answer) {
+            case 1:
+                Cli.greeting();
+            case 2: SecondGame.even();
+            case 0:
+                System.exit(0);
+        }
     }
 }
---
+++ app/src/main/java/hexlet/code/SecondGame.java
@@ -0,0 +1,31 @@
+package hexlet.code;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class SecondGame {
+    public static void even() {
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+        String name = Cli.greeting();
+        System.out.println("Answer 'yes' if number even otherwise answer 'no'.");
+
+        int round = 0;
+        while (round < 3) {
+            int num = random.nextInt(100) + 1;
+            System.out.println("Question: " + num);
+            String answer = scanner.nextLine();
+            
+            if ((num % 2 == 0 && answer.equalsIgnoreCase("yes")) || (num % 2 == 1 && answer.equalsIgnoreCase("no"))) {
+                System.out.println("Correct!");
+                round++;
+            } else {
+                System.out.println("Wrong!");
+                break;
+            }
+        }
+        if (round == 3) {
+            System.out.println("Congratulations, " + name);
+        }
+    }
+}
