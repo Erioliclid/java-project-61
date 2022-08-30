60b6ec81bd6b45a07596ba964b0c6c5a8e41fb13
Erioliclid <erioliclid@gmail.com>
2022-08-30 17:56:03 +0300

    create new class

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,15 +3,9 @@
  */
 package hexlet.code;
 
-import java.util.Scanner;
-
 public class App {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Welcome to the Brain Games!");
-        System.out.print("May I have your name? ");
-        String name = scanner.nextLine();
-        System.out.println("Hello, "+name+"!");
+        Cli.greeting();
     }
 }
